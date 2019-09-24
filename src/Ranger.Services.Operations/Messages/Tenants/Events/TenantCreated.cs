24c6cca34301b1a3b5778e9cf0f98835ab333426
Nick Cromwell <cromwellnick@gmail.com>
2019-09-24 01:05:54 -0400

    Registration Key support

--- src/Ranger.Services.Operations/Messages/Tenants/Events/TenantCreated.cs
+++ src/Ranger.Services.Operations/Messages/Tenants/Events/TenantCreated.cs
@@ -5,19 +5,19 @@ namespace Ranger.Services.Operations.Messages.Tenants
     [MessageNamespace("tenants")]
     public class TenantCreated : IEvent
     {
-        public TenantCreated(CorrelationContext correlationContext, string domainName, string databaseUsername, string databasePassword, NewTenantOwner owner)
+        public TenantCreated(string domainName, string databaseUsername, string databasePassword, string registrationKey, NewTenantOwner owner)
         {
-            this.CorrelationContext = correlationContext;
             this.DomainName = domainName;
             this.DatabaseUsername = databaseUsername;
             this.DatabasePassword = databasePassword;
+            this.RegistrationKey = registrationKey;
             this.Owner = owner;
 
         }
-        public CorrelationContext CorrelationContext { get; }
         public string DomainName { get; }
         public string DatabaseUsername { get; }
         public string DatabasePassword { get; }
+        public string RegistrationKey { get; }
         public NewTenantOwner Owner { get; }
     }
 }
