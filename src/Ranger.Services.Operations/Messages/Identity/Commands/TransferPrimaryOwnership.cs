74efd50d0a51744f8cbbb152278cc18975a3387e
Nick Cromwell <cromwellnick@gmail.com>
2020-02-16 19:03:54 -0500

    WIP Transfer Primary Ownership

---
+++ src/Ranger.Services.Operations/Messages/Identity/Commands/TransferPrimaryOwnership.cs
@@ -0,0 +1,36 @@
+using Ranger.RabbitMQ;
+
+namespace Ranger.Services.Operations.Messages.Identity.Commands
+{
+    [MessageNamespace("operations")]
+    public class TransferPrimaryOwnership : ICommand
+    {
+        public TransferPrimaryOwnership(string commandingUserEmail,
+            string transferUserEmail,
+            string domain)
+        {
+            if (string.IsNullOrWhiteSpace(commandingUserEmail))
+            {
+                throw new System.ArgumentException($"{nameof(commandingUserEmail)} was null or whitespace.");
+            }
+
+            if (string.IsNullOrWhiteSpace(transferUserEmail))
+            {
+                throw new System.ArgumentException($"{nameof(transferUserEmail)} was null or whitespace.");
+            }
+
+            if (string.IsNullOrWhiteSpace(domain))
+            {
+                throw new System.ArgumentException($"{nameof(domain)} was null or whitespace.");
+            }
+
+            CommandingUserEmail = commandingUserEmail;
+            TransferUserEmail = transferUserEmail;
+            Domain = domain;
+        }
+
+        public string CommandingUserEmail { get; private set; }
+        public string TransferUserEmail { get; private set; }
+        public string Domain { get; private set; }
+    }
+}
---
+++ src/Ranger.Services.Operations/Sagas/TransferPrimaryOwnershipSaga.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Threading.Tasks;
+using Chronicle;
+using Microsoft.Extensions.Logging;
+using Ranger.InternalHttpClient;
+using Ranger.RabbitMQ;
+using Ranger.Services.Operations;
+using Ranger.Services.Operations.Messages.Identity.Commands;
+
+namespace Ranger.Services.Operations.Sagas
+{
+    public class TransferPrimaryOwnershipSaga : BaseSaga<TransferPrimaryOwnershipSaga, TransferPrimaryOwnershipData>,
+        ISagaStartAction<TransferPrimaryOwnershipSagaInitializer>
+    {
+        const string EVENT_NAME = "transfer-primary-ownership";
+        private readonly IBusPublisher busPublisher;
+        private readonly ILogger<TransferPrimaryOwnershipSaga> logger;
+        private readonly ITenantsClient tenantsClient;
+
+        public TransferPrimaryOwnershipSaga(IBusPublisher busPublisher, ITenantsClient tenantsClient, ILogger<TransferPrimaryOwnershipSaga> logger) : base(tenantsClient, logger)
+        {
+            this.busPublisher = busPublisher;
+            this.tenantsClient = tenantsClient;
+            this.logger = logger;
+        }
+
+        public async Task CompensateAsync(TransferPrimaryOwnershipSagaInitializer message, ISagaContext context)
+        {
+            Data.Domain = message.Domain;
+            Data.Initiator = message.CommandingUserEmail;
+            Data.TransferUserEmail = message.TransferUserEmail;
+            await Task.Run(() => this.busPublisher.Send(new TransferPrimaryOwnership(message.CommandingUserEmail, message.TransferUserEmail, message.Domain), CorrelationContext.FromId(Guid.Parse(context.SagaId))));
+        }
+
+        public async Task HandleAsync(TransferPrimaryOwnershipSagaInitializer message, ISagaContext context)
+        {
+            await Task.Run(() =>
+               logger.LogInformation("Calling compensate for TransferPrimaryOwnershipSagaInitializer.")
+            );
+        }
+    }
+
+    public class TransferPrimaryOwnershipData : BaseSagaData
+    {
+        public string TransferUserEmail { get; set; }
+    }
+}
