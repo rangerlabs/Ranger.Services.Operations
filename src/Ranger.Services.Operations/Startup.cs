7f759b0cc91e7b8057f87d1b5bc885ff36fceada
Nick Cromwell <cromwellnick@gmail.com>
2020-04-11 23:29:55 -0400

    Fix duplicate ProjectsHttpClient registration

--- src/Ranger.Services.Operations/Startup.cs
+++ src/Ranger.Services.Operations/Startup.cs
@@ -64,7 +64,7 @@ namespace Ranger.Services.Operations
 
             services.AddTenantsHttpClient("http://tenants:8082", "tenantsApi", "cKprgh9wYKWcsm");
             services.AddProjectsHttpClient("http://projects:8086", "projectsApi", "usGwT8Qsp4La2");
-            services.AddProjectsHttpClient("http://identity:5000", "IdentityServerApi", "89pCcXHuDYTXY");
+            services.AddIdentityHttpClient("http://identity:5000", "IdentityServerApi", "89pCcXHuDYTXY");
 
             services.AddDbContext<OperationsDbContext>(options =>
             {
